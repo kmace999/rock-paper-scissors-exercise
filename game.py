be53fb693cbda453339d6cf07639d3b8fe8db09b
kmace999 <77734772+kmace999@users.noreply.github.com>
2021-02-02 21:01:53 -0500

    wrote the game code
    still need to edit according to assignment guidelines, maybe add a .env thing?

--- game.py
+++ game.py
@@ -1,3 +1,60 @@
-# game.py
+#game.py
+#rock, paper, scissors game
 
-print("Rock, Paper, Scissors, Shoot!")
+import time
+import random
+
+username = 'Player One'
+
+
+#fetch new username from .env?
+
+def rpsgame():
+    delay = 3
+    print("-------------------")
+    print("Welcome",username,"to my Rock-Paper-Scissors game...")
+    print("-------------------")
+    computerChoice =random.choice(['rock','paper','scissors'])
+
+    terminology = True
+
+    while terminology:
+        userplay = input("Please choose either 'rock', 'paper', or 'scissors':")
+        if userplay=='rock' or userplay=='paper' or userplay== 'scissors':
+            terminology=False
+
+
+    if computerChoice==userplay:
+        result = "It's a tie!"
+    elif computerChoice=='rock':
+        if userplay=='scissors':
+            result = "Oh, the computer won. It's ok. Try again!"
+        if userplay=='paper':
+            result= "Congratulations, you beat the computer and won! :)"
+    elif computerChoice=='paper':
+        if userplay=='scissors':
+            result= "Congratulations, you beat the computer and won! :)"
+        if userplay=='rock':
+            result = "Oh, the computer won. It's ok. Try again!"
+    elif computerChoice=='scissors':
+        if userplay=='rock':
+            result= "Congratulations, you beat the computer and won! :)"
+        if userplay=='paper':
+            result = "Oh, the computer won. It's ok. Try again!"
+
+
+
+    print("Rock, Paper, Scissors, Shoot!")
+    print("You chose:", userplay)
+    time.sleep(delay)
+    print("The computer chose: ",computerChoice)
+    print("-------------------")
+    print(result)
+    print("-------------------")
+    print("Thanks for playing. Please play again!")
+
+
+
+#print("Rock, Paper, Scissors, Shoot!")
+
+rpsgame()
